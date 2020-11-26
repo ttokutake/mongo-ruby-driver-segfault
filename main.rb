7f29e993e0515d0e63c50fc9b0725d59c898b03e
ttokutake <tadatoshi.tokutake@quipper.com>
2020-11-26 22:11:01 +0900

    Add other versions' Ruby and mongo-ruby-driver to compare them

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ require "bundler/inline"
 gemfile(true) do
   source "https://rubygems.org"
 
-  gem "mongo", '2.11.0' # 2.10.5 does not cause the segfault.
+  gem "mongo", '2.11.0' # causes the segfault.
+  # gem "mongo", '2.10.5' # does not cause the segfault.
+  # gem "mongo", '2.13.1' # with Ruby 3.0.0-preview1 does not cause the segfault.
 end
 
 uri = 'mongodb+srv://root:password123@mongodb.cluster.local/admin?ssl=false'
